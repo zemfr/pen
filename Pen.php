c0747c0d14de50e80f6e49d827c1017d755958d8
zemfr <zemfr86@gmail.com>
2015-04-14 21:50:15 +0300

    Auto pen

--- Pen.php
+++ Pen.php
@@ -8,8 +8,46 @@
 
 namespace Zemfr;
 
+use Zemfr\Exceptions\PenEnabledException;
 
+
+/**
+ * Class Pen
+ * @package Zemfr
+ */
 class Pen extends PenAbstract
 {
+    /**
+     * @var
+     */
+    protected $enabled;
+
+    /**
+     * @param bool $enabled
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param string $text
+     * @throws Exceptions\PenEnabledException
+     */
+    public function write($text)
+    {
+        if (!$this->getEnabled()) {
+            throw new PenEnabledException();
+        }
+        parent::write($text);
+    }
 
 }
